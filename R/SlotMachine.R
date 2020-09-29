57c288b8aa315c30f9f899564f3d8e2a35f46500
Ziyad Almatrafi <51778982+ZiyadAlmatrafi@users.noreply.github.com>
2020-09-29 10:34:44 +0300

    Update SlotMachine.R

--- R/SlotMachine.R
+++ R/SlotMachine.R
@@ -1,4 +1,4 @@
-# Set Sybols of the slot machine
+# Set Symbols of the slot machine
 get_symbols <- function() {
   wheel <- c("DD","7","BBB","BB","B","C","0")
   sample(wheel,size=3,replace=TRUE,prob=c(0.03,0.03,0.06,0.1,0.25,0.01,0.52))
